76e157ad066984a46b0016a4c5230c98ce9fb6d4
Thiago Carnaes <thiago.carnaes@gmail.com>
2019-12-09 19:40:24 -0300

    section 97 and 98

---
+++ section07/98/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="container">
+        <header-status></header-status>
+        <div class="row">            
+            <servers></servers>
+            <server-detail></server-detail>
+        </div>
+        <footer-status></footer-status>
+    </div>
+</template>
+
+<script>
+    import HeaderStatus from './HeaderStatus.vue';
+    import FooterStatus from './FooterStatus.vue';
+    import Servers from './Servers.vue';
+    import ServerDetail from './ServerDetail.vue';
+    export default {
+        components: {
+            'header-status' : HeaderStatus,
+            'footer-status' : FooterStatus,
+            'servers' : Servers,
+            'server-detail' : ServerDetail
+        }
+    }
+</script>
+
+<style>
+
+</style>
---
+++ section07/98/src/HeaderStatus.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <div class="row">
+            <div class="col-xs-12">
+                <header>
+                    <h1>Server Status</h1>
+                </header>
+            </div>
+        </div>
+        <hr>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style>
+
+</style>
